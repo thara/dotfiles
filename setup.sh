b2f1fea224088b69f61edfe6a85608b19489597f
Tomochika Hara <hara@wonderpla.net>
2013-10-10 09:55:21 +0900

    .vrapperrc追加

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=( ".vimrc" ".gvimrc" ".vim" ".ghci" ".todo.cfg" )
+DOT_FILES=( ".vimrc" ".gvimrc" ".vim" ".ghci" ".todo.cfg" ".vrapperrc")
 
 for file in ${DOT_FILES[@]}
 do
