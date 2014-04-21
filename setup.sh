03af5bc1640096e32e775435eadcfc88097dd95c
Tomochika Hara <tomochikahara@gmail.com>
2014-04-21 20:38:08 +0900

    Added gemrc

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=( ".zshrc" ".vimrc" ".gvimrc" ".vim" ".ghci" ".todo.cfg" ".vrapperrc" ".gitconfig" ".tmux.conf")
+DOT_FILES=( ".zshrc" ".vimrc" ".gvimrc" ".vim" ".ghci" ".todo.cfg" ".vrapperrc" ".gitconfig" ".tmux.conf" ".gemrc")
 
 for file in ${DOT_FILES[@]}
 do
