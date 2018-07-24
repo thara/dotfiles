c7edb43da410e888f96513c34e51faf3bfcc76fa
Tomochika Hara <git@thara.jp>
2018-07-24 10:09:54 +0900

    Add hammerspoon hotkey

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -25,3 +25,17 @@ hs.hotkey.bind({"ctrl"}, "8", function()
   f.h = max.h - 160
   win:setFrame(f)
 end)
+
+-- ウインドウを中央に配置 2
+hs.hotkey.bind({"ctrl"}, "9", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + 400
+  f.y = max.y + 50
+  f.w = 1200
+  f.h = max.h - 100
+  win:setFrame(f)
+end)
