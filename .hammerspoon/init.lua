a0b79ec31d44862c924cbbd9fcf5b2e7eb75676d
Tomochika Hara <github.thara.dev>
2020-09-09 10:54:12 +0900

    Fix centering windows by hammerspoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -19,11 +19,11 @@ hs.hotkey.bind({"ctrl"}, "8", function()
   local screen = win:screen()
   local max = screen:frame()
 
-  f.x = max.x + 100
-  f.y = max.y + 80
   f.w = max.w - 200
   f.h = max.h - 160
   win:setFrame(f)
+
+  hs.window.focusedWindow():centerOnScreen(nil, true)
 end)
 
 -- ウインドウを中央に配置 2
@@ -33,11 +33,11 @@ hs.hotkey.bind({"ctrl"}, "9", function()
   local screen = win:screen()
   local max = screen:frame()
 
-  f.x = max.x + 300
-  f.y = max.y + 50
   f.w = 1400
   f.h = max.h - 100
   win:setFrame(f)
+
+  hs.window.focusedWindow():centerOnScreen(nil, true)
 end)
 
 -- ウインドウを中央に配置 3
@@ -47,11 +47,11 @@ hs.hotkey.bind({"ctrl"}, "7", function()
   local screen = win:screen()
   local max = screen:frame()
 
-  f.x = max.x + 500
-  f.y = max.y + 50
-  f.w = 1000
+  f.w = 1800
   f.h = max.h - 100
   win:setFrame(f)
+
+  hs.window.focusedWindow():centerOnScreen(nil, true)
 end)
 
 hs.hotkey.bind({"ctrl"}, "1", function()
