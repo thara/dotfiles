0f1b508f0de259674c2911720fb6a6d36991d4e2
Tomochika Hara <github@thara.dev>
2021-05-22 12:12:05 +0900

    :wrench: Update hammerspoon config

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -88,3 +88,16 @@ hs.hotkey.bind({"cmd", "ctrl"}, "Right", function()
   f.h = max.h
   win:setFrame(f)
 end)
+
+hs.hotkey.bind({"cmd", "ctrl"}, "Down", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 4)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
