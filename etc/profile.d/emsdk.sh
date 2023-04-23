63829b92ae7f714be63d0347067e55f58f017042
Tomochika Hara <git@thara.dev>
2023-04-24 00:29:51 +0900

    Set up emsdk

---
+++ etc/profile.d/emsdk.sh
@@ -0,0 +1,5 @@
+export PATH="$HOME/src/github.com/emscripten-core/emsdk:$PATH"
+
+export EMSDK_QUIET=1
+
+[ -f $HOME/src/github.com/emscripten-core/emsdk/emsdk_env.sh ] && source $HOME/src/github.com/emscripten-core/emsdk/emsdk_env.sh
